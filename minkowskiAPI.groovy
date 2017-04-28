3a099ec11b1b30ff6bb9f3098ad4dc1df2b4e0d8
Kevin Harrington <mad.hephaestus@gmail.com>
2017-04-28 11:56:22 -0400

    minkowski examples

--- minkowskiAPI.groovy
+++ minkowskiAPI.groovy
@@ -1,1 +1,14 @@
 //Your code here
+CSG startCube = new Cube(100).toCSG()
+CSG cubeToFit = new Cube(50).toCSG()
+				.movez(startCube.getMaxZ())
+				.setColor(javafx.scene.paint.Color.RED)
+CSG clearenceFit = startCube
+				.minkowskiDifference(
+					cubeToFit,// the part we want to fit into a cutout
+					5.0// the offset distance to fit
+					)
+CSG clearenceObject = cubeToFit.toolOffset(5.0)
+					.movey(100)
+
+return [clearenceFit,cubeToFit,clearenceObject]
